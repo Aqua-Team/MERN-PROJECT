82329749eb151572ec51e79d836dd510c4420315
shivkumar38 <80088138+shivkumar38@users.noreply.github.com>
2021-04-12 15:58:13 +0530

    Update router.js

--- server/router/router.js
+++ server/router/router.js
@@ -1,1 +1,16 @@
 const router = express.Router();
+var mysql = require('mysql');  
+var con = mysql.createConnection({  
+host: "localhost",  
+user: "root",  
+password: "12345",  
+database: "javatpoint"  
+});  
+con.connect(function(err) {  
+if (err) throw err;  
+var sql = "DELETE FROM employees WHERE city = 'Delhi'";  
+con.query(sql, function (err, result) {  
+if (err) throw err;  
+console.log("Number of records deleted: " + result.affectedRows);  
+});  
+});  
